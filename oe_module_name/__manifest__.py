d2cebeecc7a764036a9366599fb70550685f3b86
Ayaz Hasan <m.ayaz@odooerp.ae>
2026-04-28 17:48:37 +0500

    [ADD] standard odoo module

---
+++ oe_module_name/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Module NAme",
+    "summary": """ Module Summary """,
+    "author": "Oakland.ae",
+    "website": "https://www.oaklandmena.com/",
+    "category": "",
+    "version": "",
+    "depends": [],
+    
+    
+    "data": [
+       
+    ],
+    "images": [""],
+    "license": "OPL-1",
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
